d2a0f4c5eb01f312107efb552880abdf02f181c2
rahel <102800020+rwinterschlaf@users.noreply.github.com>
2022-12-15 15:46:23 +0100

    9 tests passing - best case and worst case pipeline
    Updated the bad URL on the best case tests, implemented three worst case pipeline tests with a good URL. Output dataframe has some empty rows which will need to to be fixed in next commit.

--- newsfeedback/main.py
+++ newsfeedback/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import trafilatura, re
 from trafilatura import feeds
 from loguru import logger as log
-
+from bs4 import BeautifulSoup
 
 @click.command()
 @click.argument("name", type=str)
@@ -46,13 +46,14 @@ def get_article_metadata_best_case(article_url, metadata_wanted):
     """
     downloaded = trafilatura.fetch_url(article_url)
     metadata = trafilatura.bare_extraction(downloaded, only_with_metadata=True, include_links=True)
-    if metadata is not None:
+    homepage_url_not_article = re.match(r".*?\/(?!.+)",article_url) 
+    if homepage_url_not_article is not None or metadata is None:
+        metadata = []
+    else:
         dict_keys = list(metadata.keys())
         dict_keys_to_pop = [key for key in dict_keys if key not in metadata_wanted]
         for key in dict_keys_to_pop: 
             metadata.pop(key, None)
-    else:
-        metadata = []
     get_article_metadata_best_case.metadata = metadata
     return get_article_metadata_best_case.metadata
     
@@ -63,13 +64,81 @@ def get_article_urls_and_metadata_best_case(homepage_url, metadata_wanted):
     get_article_urls_best_case(homepage_url)
     article_url_list = get_article_urls_best_case.article_url_list
     article_list = []
-    for article_url in article_url_list:
-        get_article_metadata_best_case(article_url, metadata_wanted)
-        metadata =  get_article_metadata_best_case.metadata
-        article_list.append(metadata)
+    if len(article_url_list) != 0:
+        for article_url in article_url_list:
+            get_article_metadata_best_case(article_url, metadata_wanted)
+            metadata =  get_article_metadata_best_case.metadata
+            article_list.append(metadata)
     df = pd.DataFrame.from_dict(article_list)
     get_article_urls_and_metadata_best_case.df = df
     return get_article_urls_and_metadata_best_case.df
-        
+
+def get_article_urls_worst_case(homepage_url):
+    article_list = feeds.find_feed_urls(homepage_url)
+    article_url_list = []
+    downloaded = trafilatura.fetch_url(homepage_url)
+    soup = BeautifulSoup(downloaded, 'html.parser')
+    for a in soup.find_all('a'):
+        href = a.get('href')
+        http_check = re.search(r'(http)', f'{href}')
+        if href != None:
+            if http_check == None:
+                http_url = f"{homepage_url}" + f"{href}"
+                double_slash_check = re.search(r"(?<!https:)(//)", http_url)
+                if double_slash_check:
+                    http_url = re.sub(r"(?<!https:)(//)", r"/", http_url)
+                double_de_check = re.search(r"/de/de/", http_url)
+                if double_de_check:
+                    http_url = re.sub(r"/de/de/", r"/de/", http_url)
+                article_url_list.append(http_url)
+            else:
+                homepage_de = re.search(r'(https://www\..+?\.\w{2,3}/de/)', homepage_url)
+                if homepage_de:
+                    homepage_split = homepage_de.group(0)
+                else:
+                    homepage_split = re.search(r'(https://www\..+?\.\w{2,3})', homepage_url).group(0)
+                homepage_check = re.search(fr'{homepage_split}/.+', href)
+                if homepage_check:
+                    article_url_list.append(href)
+    article_url_list = list(dict.fromkeys(article_url_list))
+    article_url_list = list(filter(lambda item: item is not None, article_url_list))
+    if len(article_url_list) != 0:
+        print(f'{len(article_url_list)} links have been found.\r')
+    get_article_urls_worst_case.article_url_list = article_url_list
+    return get_article_urls_worst_case.article_url_list
+
+def get_article_metadata_worst_case(article_url, metadata_wanted):
+    downloaded = trafilatura.fetch_url(article_url)
+    if downloaded != None:
+        metadata = trafilatura.bare_extraction(downloaded, only_with_metadata=True, include_links=True)
+        if metadata != None:
+            dict_keys = list(metadata.keys()) # look into refactoring the following bit of code at some point
+            dict_keys_to_pop = [key for key in dict_keys if key not in metadata_wanted]
+            for key in dict_keys_to_pop: 
+                metadata.pop(key, None)
+        else:
+            metadata = {}
+    else:  
+        metadata = {}
+    metadata = {metadata_key: metadata_value for metadata_key, metadata_value in metadata.items() if metadata_key and metadata_value}
+    get_article_metadata_worst_case.metadata = metadata
+    return get_article_metadata_worst_case.metadata
+
+def get_article_urls_and_metadata_worst_case(homepage_url, metadata_wanted): # might be a bit slow
+    get_article_urls_worst_case(homepage_url)
+    article_url_list = get_article_urls_worst_case.article_url_list
+    # clear out none
+    article_list = []
+    for article_url in article_url_list:
+        if article_url != None:
+            get_article_metadata_worst_case(article_url, metadata_wanted)
+            metadata =  get_article_metadata_worst_case.metadata
+            article_list.append(metadata)
+    print(f'{homepage_url}: {len(article_list)} articles have been found.\r')
+    df = pd.DataFrame.from_dict(article_list)
+    print(df)
+    get_article_urls_and_metadata_worst_case.df = df
+    return get_article_urls_and_metadata_worst_case.df
+
 if __name__ == "main":
     cli()
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from newsfeedback.main import cli_implementation, get_article_urls_best_case, get_article_metadata_best_case, get_article_urls_and_metadata_best_case
+from newsfeedback.main import cli_implementation, get_article_urls_best_case, get_article_metadata_best_case, get_article_urls_and_metadata_best_case, get_article_urls_worst_case, get_article_metadata_worst_case, get_article_urls_and_metadata_worst_case
    
 class TestBestCasePipeline(object):
     def test_GetArticleURLs_BestCase_GoodURL(self):
@@ -45,7 +45,7 @@ class TestBestCasePipeline(object):
         """ Asserts whether the desired metadata (in this case: Title, Date, URL, Description) 
         fail to be extracted from a webpage that is not an article
         """
-        article_url = "https://leibniz-hbi.de/en"
+        article_url = "https://www.badische-zeitung.de/"
         metadata_wanted = ['title', 'date', 'url', 'description']
         actual = get_article_metadata_best_case(article_url, metadata_wanted)
         expected = 0
@@ -64,19 +64,56 @@ class TestBestCasePipeline(object):
         not_expected = 0
         message = ("get_article_metadata_best_case(article_url, metadata_wanted) "
                    "returned {0}, which is identical "
-                   "to {1}".format(len(actual),not_expected))
-        assert len(actual) != not_expected, message        
+                   "to {1}".format(actual.shape[0],not_expected))
+        assert actual.shape[0] != not_expected, message        
         
     def test_GetArticleURLAndMetadata_TitleDateURLDescription_BestCase_BadURL(self):
         """ Asserts whether the desired metadata (in this case: Title, Date, URL, Description) 
         fail to be extracted from an article whose URL failed to be extracted from a "worst case" homepage
         """
-        homepage_url = "https://leibniz-hbi.de/en"
+        homepage_url = "https://www.badische-zeitung.de/"
         metadata_wanted = ['title', 'date', 'url', 'description']
         actual = get_article_urls_and_metadata_best_case(homepage_url, metadata_wanted)
         expected = 0
         message = ("get_article_metadata_best_case(article_url, metadata_wanted) "
                    "returned {0}, instead "
-                   "of {1}".format(len(actual),expected))
-        assert len(actual) == expected, message        
+                   "of {1}".format(actual.shape[0],expected))
+        assert actual.shape[0] == expected, message        
         
+class TestWorstCasePipeline(object):
+    def test_GetArticleURLs_WorstCase_GoodURL(self):
+        """ comment here
+        """
+        homepage_url = "https://www.badische-zeitung.de/"
+        actual = get_article_urls_worst_case(homepage_url)
+        not_expected = 0
+        message = ("get_article_urls_worst_case(homepage_url) "
+                   "returned {0}, which is identical "
+                   "to {1}".format(len(actual),not_expected))
+        assert len(actual) != not_expected, message
+
+    ### would be good to find an URL without any a hrefs to test the opposing case
+
+    def test_GetArticleMetadata_TitleDateURLDescription_WorstCase_GoodURL(self):
+        """ comment here
+        """
+        article_url = "https://www.badische-zeitung.de/unwetterwarnung-aufgehoben-aber-schnee-und-eis-sollen-ueber-nacht-zurueckkehren"
+        metadata_wanted = ['title', 'date', 'url', 'description']
+        actual = get_article_metadata_worst_case(article_url, metadata_wanted)
+        not_expected = 0
+        message = ("get_article_metadata_worst_case(article_url, metadata_wanted) "
+                   "returned {0}, which is identical "
+                   "to {1}".format(len(actual),not_expected))
+        assert len(actual) != not_expected, message
+
+    def test_GetArticleURLAndMetadata_TitleDateURLDescription_WorstCase_GoodURL(self):
+        """ comment here
+        """
+        homepage_url = "https://www.badische-zeitung.de/"
+        metadata_wanted = ['title', 'date', 'url', 'description']
+        actual = get_article_urls_and_metadata_worst_case(homepage_url, metadata_wanted)
+        not_expected = 0
+        message = ("get_article_metadata_worst_case(article_url, metadata_wanted) "
+                   "returned {0}, which is identical "
+                   "to {1}".format(actual.shape[0],not_expected))
+        assert actual.shape[0] != not_expected, message          
